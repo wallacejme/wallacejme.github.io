a930ba9c155187c3782f27a3aef9414ea2da576b
Wallace Medeiros <wallacejme@gmail.com>
2022-12-15 14:30:19 -0300

    Add click trigger to definition translations
    This will allow people on mobile to use it too

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,11 @@
 let definitions = {
     definition_1 : {
+        currentLanguage: "pt_BR",
         pt_BR: "um desenvolvedor de software brasileiro",
         en: "a brazilian software developer"
     },
     definition_2: {
+        currentLanguage: "pt_BR",
         pt_BR: "um bocado de tudos, nadas e meios termos",
         en: "a bunch of everythings, nothings and in-betweens (whatever that means)"
     }
@@ -14,11 +16,25 @@ function getId (event) {
 }
 
 function switchDefinitionLanguage (defId, lang) {
+    let definition = definitions[defId]
+    if (definition.currentLanguage === lang)
+        return
+
     $( `#${defId}` )
         .hide()
-        .text(definitions[defId][lang])
+        .text(definition[lang])
         .toggleClass("fst-italic text-primary")
-        .fadeIn(150)
+        .fadeIn(100)
+
+    definition.currentLanguage = lang
+}
+
+function toggleDefinitionLanguage (defId) {
+    let currentLang = definitions[defId].currentLanguage
+    if (currentLang === "pt_BR")
+        switchDefinitionLanguage(defId, "en")
+    else
+        switchDefinitionLanguage(defId, "pt_BR")
 }
 
 $( document ).ready(function() {
@@ -34,5 +50,10 @@ $( document ).ready(function() {
             switchDefinitionLanguage(getId(event), "pt_BR")
         }
     )
-    
+
+    // Adding support for mobile
+    $("#definitions li").click(function ( event ) {
+        toggleDefinitionLanguage(getId(event))
+    })
+
 });
